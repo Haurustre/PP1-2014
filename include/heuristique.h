57293b7b0f3024b783086d382f949c1d653ac5cf
Jimmy Gustan <jgustan@emi.u-bordeaux1.fr>
2014-03-05 12:16:58 +0100

    ajout interface et implémentation heuristique

---
+++ include/heuristique.h
@@ -0,0 +1,13 @@
+#ifndef __HEURISTIQUE_H__
+#define __HEURISTIQUE_H__
+
+#include <stdbool.h>
+
+double sommetsDistanceMatrice(int , int , matrice )
+int sommetPlusProche(int , matrice , int *)
+int * heuristique(matrice ,)
+void deleteParcouru( int * )
+
+
+
+#endif
